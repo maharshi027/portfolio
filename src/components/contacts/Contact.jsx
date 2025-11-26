4240c195e906ce7a7a86a58277396afc63e3bc99
Harshit <harshkush15sep@gmail.com>
2025-11-26 22:03:01 +0530

    style: standardize import statement formatting in Contact component

--- src/components/contacts/Contact.jsx
+++ src/components/contacts/Contact.jsx
@@ -1,5 +1,6 @@
 import React, { useRef } from 'react';
-import emailjs from '@emailjs/browser';
+import emailjs from "@emailjs/browser";
+
 import './contact.css';
 import { MdEmail } from "react-icons/md";
 import { FaWhatsapp } from "react-icons/fa";
